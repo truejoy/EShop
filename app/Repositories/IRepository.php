d611abd987b69be47f1de65a573f08add60826ff
truejoy <turjoybd@gmail.com>
2019-11-07 15:02:13 +0600

    try a repository example
    from https://medium.com/employbl/use-the-repository-design-pattern-in-a-laravel-application-13f0b46a3dce

---
+++ app/Repositories/IRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories;
+
+interface IRepository
+{
+    public function all();
+    public function create(array $data);
+    public function update(array $data, $id);
+    public function show($id);
+    public function delete($id);
+}
